d8657117ba1c6d71c16477eca6566cdc52c789c3
Samantha Jeet <samanthajeet@gmail.com>
2024-05-10 00:45:48 -0700

    update ts types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import { JSX, ClassAttributes, AnchorHTMLAttributes } from "react";
 
 export default function Home() {
-  function Badge(props) {
+  function Badge(
+    props: JSX.IntrinsicAttributes &
+      ClassAttributes<HTMLAnchorElement> &
+      AnchorHTMLAttributes<HTMLAnchorElement>
+  ) {
     return (
       <a
         {...props}
